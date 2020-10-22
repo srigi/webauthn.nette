d025d5c7b69f2c55307e1962bf1c4fddc98eda91
srigi <srigi@srigi.sk>
2020-10-22 19:51:20 +0200

    Implement assertion

--- src/Lib/Webauthn/CredentialSourceRepository.php
+++ src/Lib/Webauthn/CredentialSourceRepository.php
@@ -5,6 +5,7 @@ namespace App\Lib\Webauthn;
 use Nette\Database;
 use Nette\SmartObject;
 use Nette\Utils\Json;
+use Ramsey\Uuid\Uuid;
 use Webauthn\PublicKeyCredentialSource;
 use Webauthn\PublicKeyCredentialSourceRepository;
 use Webauthn\PublicKeyCredentialUserEntity;
@@ -26,12 +27,22 @@ class CredentialSourceRepository implements PublicKeyCredentialSourceRepository
 
 	public function findOneByCredentialId(string $publicKeyCredentialId): ?PublicKeyCredentialSource
 	{
-		$hwCredentials = $this->database->table(self::TABLE_HW_CREDENTIALS)
+		$hwCredential = $this->database->table(self::TABLE_HW_CREDENTIALS)
 			->where(self::COLUMN_HW_CREDENTIALS_PUBLIC_KEY_CREDENTIAL_ID, \base64_encode($publicKeyCredentialId))
 			->fetch();
 
-		if ($hwCredentials) {
-
+		if ($hwCredential) {
+			return new PublicKeyCredentialSource(
+				\base64_decode($hwCredential['public_key_credential_id'], true),
+				$hwCredential['type'],
+				Json::decode($hwCredential['transports']),
+				$hwCredential['attestation_type'],
+				\unserialize($hwCredential['trust_path']),
+				Uuid::fromString($hwCredential['aaguid']),
+				\base64_decode($hwCredential['credential_public_key'], true),
+				$hwCredential['user_handle'],
+				$hwCredential['counter'],
+			);
 		} else {
 			return null;
 		}
@@ -43,20 +54,31 @@ class CredentialSourceRepository implements PublicKeyCredentialSourceRepository
 
 	public function saveCredentialSource(PublicKeyCredentialSource $publicKeyCredentialSource): void
 	{
-		$data = [
-			'user_id' => $publicKeyCredentialSource->getUserHandle(),
-			'public_key_credential_id' => \base64_encode($publicKeyCredentialSource->getPublicKeyCredentialId()),
-			'type' => $publicKeyCredentialSource->getType(),
-			'transports' => Json::encode($publicKeyCredentialSource->getTransports()),
-			'attestation_type' => $publicKeyCredentialSource->getAttestationType(),
-			'trust_path' => Json::encode($publicKeyCredentialSource->getTrustPath()->jsonSerialize()),
-			'aaguid' => $publicKeyCredentialSource->getAaguid()->toString(),
-			'credential_public_key' => \base64_encode($publicKeyCredentialSource->getCredentialPublicKey()),
-			'user_handle' => $publicKeyCredentialSource->getUserHandle(),
-			'counter' => $publicKeyCredentialSource->getCounter(),
-		];
+		$existingHwCredential = $this->findOneByCredentialId($publicKeyCredentialSource->getPublicKeyCredentialId());
+		if ($existingHwCredential) {
+			// update counter
+			$this->database->table(self::TABLE_HW_CREDENTIALS)
+				->where('public_key_credential_id', \base64_encode($publicKeyCredentialSource->getPublicKeyCredentialId()))
+				->update([
+					'counter' => $publicKeyCredentialSource->getCounter(),
+				]);
 
-		$this->database->table(self::TABLE_HW_CREDENTIALS)
-			->insert($data);
+		} else {
+			$data = [
+				'user_id' => $publicKeyCredentialSource->getUserHandle(),
+				'public_key_credential_id' => \base64_encode($publicKeyCredentialSource->getPublicKeyCredentialId()),
+				'type' => $publicKeyCredentialSource->getType(),
+				'transports' => Json::encode($publicKeyCredentialSource->getTransports()),
+				'attestation_type' => $publicKeyCredentialSource->getAttestationType(),
+				'trust_path' => \serialize($publicKeyCredentialSource->getTrustPath()),
+				'aaguid' => $publicKeyCredentialSource->getAaguid()->toString(),
+				'credential_public_key' => \base64_encode($publicKeyCredentialSource->getCredentialPublicKey()),
+				'user_handle' => $publicKeyCredentialSource->getUserHandle(),
+				'counter' => $publicKeyCredentialSource->getCounter(),
+			];
+
+			$this->database->table(self::TABLE_HW_CREDENTIALS)
+				->insert($data);
+		}
 	}
 }

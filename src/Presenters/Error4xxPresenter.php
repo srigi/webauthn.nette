e72da876395a6873508d07dbf297b80c4d76bc28
srigi <srigi@srigi.sk>
2020-11-25 22:09:34 +0100

    Fix path to ErrorPresenter(s) templates

--- src/Presenters/Error4xxPresenter.php
+++ src/Presenters/Error4xxPresenter.php
@@ -21,7 +21,7 @@ final class Error4xxPresenter extends UI\Presenter
 	public function renderDefault(Application\BadRequestException $exception): void
 	{
 		// load template 403.latte or 404.latte or ... 4xx.latte
-		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
-		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
+		$file = __DIR__ . "/../templates/Error/{$exception->getCode()}.latte";
+		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/../templates/Error/4xx.latte');
 	}
 }

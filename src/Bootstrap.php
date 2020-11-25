5462ff3877d178acf88c6120f956d650c80e7d23
srigi <srigi@srigi.sk>
2020-11-25 21:41:31 +0100

    Setup logging via Monolog into stdout

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -8,9 +8,13 @@ class Bootstrap
 {
 	public static function boot(): Configurator
 	{
+		$appDebug = (bool) getenv('APP_DEBUG');
+		$loggingLevel = $appDebug === true ? 'DEBUG' : 'ERROR';
+		putenv("LOGGING_LEVEL=$loggingLevel");
+
 		$configurator = new Configurator;
-		$configurator->setDebugMode(getenv('APP_DEBUG') == true);
-		$configurator->enableTracy(__DIR__ . '/../logs');
+		$configurator->setDebugMode($appDebug);
+		$configurator->enableTracy(null);
 		$configurator->setTimeZone(getenv('TIMEZONE'));
 		$configurator->setTempDirectory(__DIR__ . '/../temp');
 		$configurator->addConfig(__DIR__ . '/../config/common.neon');

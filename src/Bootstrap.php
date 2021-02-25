71e293c7ea32999d99f8c17df3b9e67bf798263b
srigi <srigi@srigi.sk>
2021-02-25 08:38:41 +0100

    Setup session save path dynamically
    Azure cannot save sessions elsewhere than in /tmp in app-service 🤦‍
    Ssetup SESSION_SAVE_PATH by APP_ENV

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -9,14 +9,19 @@ class Bootstrap
 	public static function boot(): Configurator
 	{
 		$appDebug = (bool) getenv('APP_DEBUG');
-		$loggingLevel = $appDebug === true ? 'DEBUG' : 'ERROR';
-		putenv("LOGGING_LEVEL=$loggingLevel");
+		putenv(sprintf('LOGGING_LEVEL=%s', $appDebug === true ? 'DEBUG' : 'ERROR'));
+
+		// Azure cannot save sessions elsewhere than in /tmp in app-service 🤦‍
+		// setup SESSION_SAVE_PATH by APP_ENV
+		$appEnv = getenv('APP_ENV');
+		$tempDir = __DIR__ . '/../temp';
+		putenv(sprintf('SESSION_SAVE_PATH=%s', $appEnv === 'local' ? "${tempDir}/sessions" : '/tmp'));
 
 		$configurator = new Configurator;
 		$configurator->setDebugMode($appDebug);
 		$configurator->enableTracy(null);
 		$configurator->setTimeZone(getenv('TIMEZONE'));
-		$configurator->setTempDirectory(__DIR__ . '/../temp');
+		$configurator->setTempDirectory($tempDir);
 		$configurator->addConfig(__DIR__ . '/../config/common.neon');
 
 		return $configurator;
